bbd3f56103b491b9cf27e5772c00e1455b30c2ce
Koenigsgeier <50423436+Koenigsgeier@users.noreply.github.com>
2021-07-17 15:33:55 +0200

    Add files via upload

---
+++ src/app/destination-details/destination-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
+import { destinations } from '../destinations';
+
+@Component({
+  selector: 'app-destination-details',
+  templateUrl: './destination-details.component.html',
+  styleUrls: ['./destination-details.component.sass']
+})
+export class DestinationDetailsComponent implements OnInit {
+  destination: { name: string, price: number, description: string, beginning: string, duration: string } = { name: "", price: 0, description: "", beginning: "", duration: "", }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+
+  addToCart(destination: any) {
+
+    window.alert('Your holiday has been added to the cart!');
+
+    this.cartService.addToCart(destination);
+
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      let id: any = params.get('destinationId');
+      this.destination = destinations[id];
+    });
+  }
+
+}
